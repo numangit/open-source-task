31ff4eb11ac4e19781b9520e38e4144f20defd19
numangit <abdulnumanjob@gmail.com>
2023-06-25 17:27:09 +0600

    pages folder and components added

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,25 @@ import Navbar from './components/shared/Navbar'
 import './App.css'
 import { useContext } from 'react'
 import { StateContext } from './context/StateProvider';
+import Home from './pages/home/Home';
+import Orders from './pages/orders/Orders';
 
 function App() {
+
   //location state
   const { location } = useContext(StateContext);
 
   return (
     <div className='h-screen bg-gradient-to-r from-indigo-400 to-blue-400'>
+
       <Navbar />
+
       {
         location === 'home'
-          ? <h1 className='text-5xl font-bold'>HOME</h1>
-          : <h1>NOThome</h1>
+          ? <Home />
+          : <Orders />
       }
+
     </div>
   )
 }
