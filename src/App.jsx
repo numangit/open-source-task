b5579b929c26a9edb86edca93ab94aa5f54ee0f3
numangit <abdulnumanjob@gmail.com>
2023-06-25 16:37:39 +0600

    navbar colors fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
 import Navbar from './components/shared/Navbar'
+import './App.css'
 
 function App() {
   return (
-    <>
+    <div className='h-screen bg-gradient-to-r from-indigo-400 to-blue-400'>
       <Navbar />
-
-    </>
+      <div className='h-[400px] w-1/4'></div>
+    </div>
   )
 }
 
