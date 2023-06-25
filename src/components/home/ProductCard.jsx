e7bee7d686e4451abd3ddd982e15dcc724b354fe
numangit <abdulnumanjob@gmail.com>
2023-06-25 18:41:41 +0600

    fake data created

--- src/components/home/ProductCard.jsx
+++ src/components/home/ProductCard.jsx
@@ -1,6 +1,6 @@
 const ProductCard = () => {
   return (
-    <a href="#" className="block rounded-lg p-4 shadow-sm shadow-indigo-100 bg-slate-50">
+    <div className="block rounded-lg p-4 shadow-sm shadow-indigo-100 bg-slate-50">
       <img
         alt="Home"
         src="https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
@@ -80,7 +80,7 @@ const ProductCard = () => {
           </div>
         </div>
       </div>
-    </a>
+    </div>
 
   );
 };
---
+++ src/data/data.js
@@ -0,0 +1,83 @@
+export const products = [
+  {
+    "id": 1,
+    "name": "Serenity Blue Ray Glasses",
+    "description": "Protect your eyes from harmful blue light with these stylish Serenity glasses. They feature a lightweight frame and blue light filtering lenses.",
+    "price": 29.99,
+    "image": "https://lunettes.com.bd/wp-content/uploads/2020/08/ASTER-side.jpg",
+    "delivery_option": ["Regular Delivery", "Express Delivery"]
+  },
+  {
+    "id": 2,
+    "name": "Luna Blue Light Blockers",
+    "description": "Experience ultimate comfort and eye protection with Luna Blue Light Blockers. These glasses are designed to reduce eye strain and improve sleep quality.",
+    "price": 39.99,
+    "image": "https://lunettes.com.bd/wp-content/uploads/2020/08/ASTER-front.jpg",
+    "delivery_option": ["Regular Delivery", "Express Delivery"]
+  },
+  {
+    "id": 3,
+    "name": "Eclipse Anti-Blue Ray Glasses",
+    "description": "Eclipse glasses combine style and functionality to shield your eyes from harmful blue light. Their ergonomic design ensures a comfortable fit all day long.",
+    "price": 34.99,
+    "image": "https://lunettes.com.bd/wp-content/uploads/2018/11/Front-23.jpg",
+    "delivery_option": ["Regular Delivery", "Express Delivery"]
+  },
+  {
+    "id": 4,
+    "name": "Nova Blue Light Filtering Glasses",
+    "description": "Nova glasses are crafted with premium materials and advanced blue light filtering technology. Stay protected and stylish with these modern frames.",
+    "price": 49.99,
+    "image": "https://lunettes.com.bd/wp-content/uploads/2018/12/SideWhite.jpg",
+    "delivery_option": ["Regular Delivery", "Express Delivery"]
+  },
+  {
+    "id": 5,
+    "name": "Aurora Anti-Blue Ray Eyewear",
+    "description": "Aurora glasses offer all-day comfort and protection from blue light. These glasses are perfect for professionals who spend long hours in front of screens.",
+    "price": 27.99,
+    "image": "https://lunettes.com.bd/wp-content/uploads/2019/05/GYNK-FRONT-FRONT.jpg",
+    "delivery_option": ["Regular Delivery", "Express Delivery"]
+  },
+  {
+    "id": 6,
+    "name": "Radiance Blue Light Blockers",
+    "description": "Radiance glasses provide optimal blue light protection without compromising style. Their premium lenses effectively block harmful blue rays.",
+    "price": 32.99,
+    "image": "https://lunettes.com.bd/wp-content/uploads/2018/09/front_minified.png",
+    "delivery_option": ["Regular Delivery", "Express Delivery"]
+  },
+  {
+    "id": 7,
+    "name": "Zenith Anti-Blue Ray Glasses",
+    "description": "Zenith glasses are designed to reduce eye strain and protect your vision. Experience enhanced clarity and comfort with these anti-blue ray glasses.",
+    "price": 39.99,
+    "image": "https://lunettes.com.bd/wp-content/uploads/2018/09/Front_1.png",
+    "delivery_option": ["Regular Delivery", "Express Delivery"]
+  },
+  {
+    "id": 8,
+    "name": "Harmony Blue Light Filtering Glasses",
+    "description": "Harmony glasses combine style and functionality to combat digital eye strain. These glasses are perfect for both work and leisure.",
+    "price": 28.99,
+    "image": "https://lunettes.com.bd/wp-content/uploads/2018/11/Front-15.jpg",
+    "delivery_option": ["Regular Delivery", "Express Delivery"]
+  },
+  {
+    "id": 9,
+    "name": "Solace Anti-Blue Ray Eyewear",
+    "description": "Solace glasses feature a sleek design and advanced blue light filtering technology to protect your eyes from digital screens.",
+    "price": 36.99,
+    "image": "https://lunettes.com.bd/wp-content/uploads/2022/04/Benoit-Silver-Black-2.jpg",
+    "delivery_option": ["Regular Delivery", "Express Delivery"]
+  },
+  {
+    "id": 10,
+    "name": "Cerulean Blue Light Blocking Glasses",
+    "description": "Cerulean glasses provide effective blue light blocking to reduce eye strain and improve sleep quality. Stay stylish and protected all day long.",
+    "price": 31.99,
+    "image": "https://lunettes.com.bd/wp-content/uploads/2018/12/Buford-Red-Front-original.jpg",
+    "delivery_option": ["Regular Delivery", "Express Delivery"]
+  }
+
+]
